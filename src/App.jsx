e1e0121ace1acfdc764f164b3854852d482498f4
Vardhan Battula <170970431+Vardhanb07@users.noreply.github.com>
2025-08-01 13:35:36 +0530

    Generate pokemon images and randomize them

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import Nav from "./components/Nav.jsx";
+import Cell from "./components/Cell.jsx";
 
 export default function App() {
+  const pokemon = [
+    "wigglytuff",
+    "bibarel",
+    "torterra",
+    "shinx",
+    "baltoy",
+    "electivire",
+    "stantler",
+    "reuniclus",
+    "slakoth",
+    "mightyena",
+    "machoke",
+    "ho-oh",
+  ];
+  let set = new Set();
+  while (set.size < pokemon.length) {
+    set.add(Math.floor(Math.random() * pokemon.length));
+  }
+  const randomInd = [...set];
   return (
     <div className="p-2">
       <Nav />
+      {randomInd.map((ind) => {
+        return <Cell name={pokemon[ind]} />;
+      })}
     </div>
   );
 }
--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,1 +1,30 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import brokenImg from "../assets/broken-img.svg";
 
+export default function Cell({ name }) {
+  const [src, setSrc] = useState(brokenImg);
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`, { mode: "cors" })
+      .then((data) => {
+        return data.json();
+      })
+      .then((res) => {
+        return res["sprites"]["other"]["dream_world"]["front_default"];
+      })
+      .then((img) => {
+        setSrc(img);
+      })
+      .catch(() => {
+        setSrc(brokenImg);
+      });
+    () => {
+      setSrc(brokenImg);
+    };
+  }, [name]);
+  return (
+    <>
+      <img src={src} alt={name} />
+    </>
+  );
+}
